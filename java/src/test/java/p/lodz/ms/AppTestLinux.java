28b2192a100ef98226e3d1fadf9d14d6ef2e8665
Michał Siatkowski <atais.jr@gmail.com>
2015-01-31 13:11:01 +0100

    Linux test case

---
+++ java/src/test/java/p/lodz/ms/AppTestLinux.java
@@ -0,0 +1,25 @@
+package p.lodz.ms;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTestLinux {
+
+    private URL is;
+
+    @Before
+    public void build() throws ConfigurationException {
+	ClassLoader classloader = Thread.currentThread()
+		.getContextClassLoader();
+	is = classloader.getResource("test-linux.xml");
+    }
+
+    @Test
+    public void mainTest() throws IOException {
+	App.main(new String[] { is.toString() });
+    }
+}
