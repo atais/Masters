3c7c11f9f7526636b85e1db5fe29c3d11d9ef1aa
Michał Siatkowski <atais.jr@gmail.com>
2015-02-06 17:23:30 +0100

    Fake matsim done

--- fakematsim/src/main/java/org/matsim/run/Controler.java
+++ fakematsim/src/main/java/org/matsim/run/Controler.java
@@ -3,11 +3,14 @@ package org.matsim.run;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -40,18 +43,38 @@ public class Controler {
 	// change the time to some random value
 	// ITERS/it.100/100.tripdurations.txt
 	File duration = new File(output + "/ITERS/it.100/100.tripdurations.txt");
-	System.out.println(duration.exists());
-	// List<String> dLines = Files.readAllLines(duration.toPath(),
-	// StandardCharsets.UTF_8);
-	//
-	// // average trip duration: 1305.0772916418975 seconds = 00:21:45
-	// String line = dLines.get(dLines.size() - 1);
-	// String pattern = ".+duration: (.+) seconds.+";
-	// Double random = new Random().nextDouble();
-	// Double result = 1250 + (random * (1450 - 1250));
-	// line.replaceAll(pattern, result.toString());
-	//
-	// Files.write(duration.toPath(), dLines, Charset.defaultCharset(),
-	// StandardOpenOption.TRUNCATE_EXISTING);
+	List<String> dLines = Files.readAllLines(duration.toPath(),
+		StandardCharsets.UTF_8);
+
+	// average trip duration: 1305.0772916418975 seconds = 00:21:45
+	String line = dLines.get(dLines.size() - 1);
+	String pattern = ".*duration: (.*) seconds.*";
+	Double random = new Random().nextDouble();
+	double start = 1250.0772916418975;
+	double end = 1450.0772916418975;
+	Double result = start + (random * (end - start));
+
+	line = replaceGroup(pattern, line, 1, result.toString());
+	dLines.remove(dLines.size() - 1);
+	dLines.add(line);
+
+	Files.write(duration.toPath(), dLines, Charset.defaultCharset(),
+		StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    public static String replaceGroup(String regex, String source,
+	    int groupToReplace, String replacement) {
+	return replaceGroup(regex, source, groupToReplace, 1, replacement);
+    }
+
+    public static String replaceGroup(String regex, String source,
+	    int groupToReplace, int groupOccurrence, String replacement) {
+	Matcher m = Pattern.compile(regex).matcher(source);
+	for (int i = 0; i < groupOccurrence; i++)
+	    if (!m.find())
+		return source; // pattern not met, may also throw an exception
+			       // here
+	return new StringBuilder(source).replace(m.start(groupToReplace),
+		m.end(groupToReplace), replacement).toString();
     }
 }
